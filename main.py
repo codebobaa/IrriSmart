0d82f58b06e2bcdbec04e8d43e709aa8a4c31f99
Chloe Zhan <chloezhan@chloes-air.lan>
2022-10-23 18:47:44 -0700

    test

--- main.py
+++ main.py
@@ -228,16 +228,17 @@ try:
     Map = geemap.Map(center=[res[4],res[3]], zoom=10)
     # Add a basemap
     Map.add_basemap("SATELLITE")
-    """
+
+
     # Retrieve Earth Engine dataset
-    dem = ee.Image("USGS/SRTMGL1_003")
+    # dem = ee.Image("USGS/SRTMGL1_003")
     # Set visualization parameters
     vis_params = {
         "min": 0,
         "max": 4000,
         "palette": ["006633", "E5FFCC", "662A00", "D8D8D8", "F5F5F5"],
     }
-    """
+
     # Add the Earth Engine image to the map
     #Map.addLayer(dem, vis_params, "SRTM DEM", True, 0.5)
     # Add a colorbar to the map
